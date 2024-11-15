014621097547a7779918c724e11b2de9e465aaac
Alvarjur <aalvarojur@gmail.com>
2024-11-15 19:27:08 +0100

    .

---
+++ bets.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import math
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+import sys
+import utils
+from ruleta import numeros_vermells
+from jugadores import jugadores
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+YELLOW = (255, 255, 70)
+SALMON = (227, 70, 104)
+RED = (255, 0, 0)
+
+pygame.init()
+clock = pygame.time.Clock()
+
+# Definir la finestra
+WIDTH = 1280
+HEIGHT = 720
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Window Title')
+
+surface = pygame.Surface((WIDTH, HEIGHT))
+
+# Bucle de l'aplicació
+def main():
+    is_looping = True
+
+    surface.fill(WHITE)
+
+    while is_looping:
+        is_looping = app_events()
+        app_run()
+        app_draw()
+
+        clock.tick(60) # Limitar a 60 FPS
+
+    # Fora del bucle, tancar l'aplicació
+    pygame.quit()
+    sys.exit()
+
+# Gestionar events
+def app_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # Botó tancar finestra
+            return False
+        
+    return True
+
+# Fer càlculs
+def app_run():
+    pass
+
+# Dibuixar
+def app_draw():
+    screen.fill(WHITE)
+    utils.draw_grid(pygame, screen, 50)
+    drawBetTable((50, 50))
+    
+    pygame.display.update()
+
+def makeBet(player, posicio):
+    pass
+
+def drawBetTable(coords):
+    nums = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, ]
+    for row in range(3):
+        for cell in range(12):
+            colorFicha = RED if cell * row in numeros_vermells else BLACK
+            pygame.draw.rect(screen, GRAY, ((coords[0] + (35 * cell), coords[1] + (35 * row)), (35, 35)))
+            pygame.draw.rect(screen, BLACK, ((coords[0] + (35 * cell), coords[1] + (35 * row)), (35, 35)), 3)
+            
+            pygame.draw.circle(screen, colorFicha, ((16 + coords[0] + (35 * cell), 16 + coords[1] + (35 * row))), 10)
+
+if __name__ == "__main__":
+    main()
---
+++ ruleta.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+"""
+import pygame
+import sys
+"""
+# Definim les constants
+POSICIONS = 37
+FILES = 3
+CENTER = (450, 450)
+RADI = 350
+
+GREEN = (52, 220, 22)
+RED = (220, 22, 22)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Definim les variables globals
+numeros_vermells = [1, 3, 5, 6, 7, 12, 14, 16, 18, 19, 21, 22, 23, 25, 27, 30, 32, 34, 36]
+ruleta_distribucio = []
+"""
+pygame.init()
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption('Alejandro López - Exercici Paint')
+
+def init_ruleta():
+    global ruleta_distribucio
+
+    fila = 0
+
+    for num in range(POSICIONS):
+        # Definim el color de la casella a la ruleta
+        if num in numeros_vermells:
+            color = RED
+        elif num == 0:
+            color = GREEN
+        else:
+            color = BLACK
+
+        posicio = {
+            "number": num,                                  # Indica el propi número
+            "row": 0 if num == 0 else fila,                 # Indica la fila a la que es troba per apostar, si el número es 0, la fila també serà 0
+            "parity": "even" if num % 2 == 0 else "odd",    # Indica si el número es par o impar
+            "color": color,                                 # Indica el color del que s'ha de pintar la casella
+            "bets": {}                                      # Indica les apostes que n'hi han en aquesta casella, pot ser un diccionari similar { "player": Blau, "aposta": {"010": 3} }
+        }
+
+        # Afegim el diccionari a la ruleta
+        ruleta_distribucio.append(posicio)
+
+        fila = 1 if fila == 3 else fila + 1
+
+def draw_ruleta():
+    global ruleta_distribucio
+
+    pygame.draw.circle(screen, BLACK, CENTER, RADI, 5)
+
+
+
+# Bucle de l'aplicació
+def main():
+    is_looping = True
+
+    while is_looping:
+        is_looping = app_events()
+        app_run()
+        app_draw()
+
+        clock.tick(60) # Limitar a 60 FPS
+
+    # Fora del bucle, tancar l'aplicació
+    pygame.quit()
+    sys.exit()
+
+def app_events():
+    pass
+
+def app_run():
+    pass
+
+def app_draw():
+    global points, buttons_width, buttons_color, padding, selected_color
+
+    # Pintar el fons de blanc
+    screen.fill(WHITE)
+
+    draw_ruleta()
+
+    # Actualitzar el dibuix a la finestra
+    pygame.display.update()
+
+main()
+"""
